e804c368686aa3d2abcdb60c52a9372f2a61bbe4
Zachary Damato <zacharyd1@live.com>
2020-05-03 02:30:09 -0400

    feat: implemented the sector-selector
    Closes #3

--- modules/hungergames.ts
+++ modules/hungergames.ts
@@ -2,6 +2,7 @@ import * as Discord from "discord.js";
 import {setInterval} from "timers";
 
 import * as ReactionButtons from "../Discord-Bot-Core/src/reactionButtons";
+import * as ReactionRoles from "../Discord-Bot-Core/src/reactionRoles";
 
 import {client} from "../Discord-Bot-Core/bot";
 
@@ -12,6 +13,11 @@ const DEFAULT_CHANNEL_NAME = "minigame";
 const games = new Discord.Collection<Discord.Snowflake, Game>();
 
 //Enums are given explicit values to preserve serialization across potential future versions
+
+enum PlayerAction {
+	run = 1,
+	attack = 2
+}
 
 enum GamePhase {
 	movement = 1,
@@ -25,9 +31,32 @@ enum GameState {
 	complete = 4
 }
 
+export class Player {
+	readonly parentGame: Game;
+	readonly member: Discord.GuildMember;
+	health = 15;
+	currentSector = 1;
+	nextSector = 1;
+	nextAction = PlayerAction.run;
+
+	private constructor(data?: Partial<Player>) {
+		Object.assign(this, data);
+	}
+
+	static createNewPlayer(parentGame: Game, member: Discord.GuildMember) {
+		const player: any = {};
+		player.parentGame = parentGame;
+		player.member = member;
+		return new Player(player);
+	}
+}
+
+//TODO: Add checks to make sure people can't join games while they're in progress
+//TODO: Perhaps let people join up until the first movement phase is executed?
+
 export class Game {
 	private memberRole: Discord.Role;
-	private members = new Discord.Collection<Discord.Snowflake, Discord.GuildMember>();
+	private players = new Discord.Collection<Discord.Snowflake, Player>();
 
 	private guild: Discord.Guild;
 	private channel: Discord.TextChannel;
@@ -35,7 +64,7 @@ export class Game {
 	private phase = GamePhase.movement; 
 	private state = GameState.notStarted;
 	private nextPhaseTimer: NodeJS.Timer;
-	private phasePeriod = 60;	//The length of a game-period, in seconds.  Defaults to 1 minute
+	private phasePeriod = 3 * 60;	//The length of a game-period, in seconds.  Defaults to 3 minutes
 
 	private numSectors = 6;		//The number of sectors in the map
 
@@ -82,7 +111,9 @@ export class Game {
 			}
 		}
 
-		game.members = game.memberRole.members;
+		for(const [id, member] of game.memberRole.members) {
+			game.players.set(id, Player.createNewPlayer(game, member));
+		}
 
 		games.set(game.guild.id, game);
 
@@ -103,12 +134,72 @@ export class Game {
 		this.nextPhaseTimer = setInterval(() => this.doGameTick, this.phasePeriod * 1000);
 	}
 
+	pauseGame() {
+		if(this.state == GameState.complete) throw new Error("Cannot pause finished game");
+		if(this.state == GameState.paused) throw new Error("Cannot pause a game that is already paused");
+		if(this.state == GameState.notStarted) throw new Error("The game has not started yet");
+
+		this.state = GameState.paused;
+		clearInterval(this.nextPhaseTimer);
+	}
+
+	
+	dumpGameState(): string {
+		let str = "";
+		str += `Game state: `;
+		switch(this.state) {
+			case GameState.notStarted:
+				str += "notStarted";
+				break;
+			case GameState.inProgress:
+				str += "inProgress";
+				break;
+			case GameState.paused:
+				str += "paused";
+				break;
+			case GameState.complete:
+				str += "complete";
+				break;
+			default:
+				str += `${this.state}`;
+				break;
+		}
+		str += "\n";
+		str += `Game phase: `;
+		switch(this.phase) {
+			case GamePhase.movement:
+				str += "movement";
+				break;
+			case GamePhase.interaction:
+				str += "interaction";
+				break;
+			default:
+				str += `${this.phase}`;
+				break;
+		}
+		str += "\n";
+		str += `numSectors: ${this.numSectors}\n`;
+		str += `phasePeriod: ${this.phasePeriod}\n`;
+		str += `firstMovementPhase: ${this.isFirstMovementPhase}\n`;
+		str += `forceMovementPhase: ${this.forceMovementPhase}\n`;
+		for(const [playerID, player] of this.players) {
+			str += `>> player: ${player.member.displayName}\n`;
+			str += `>> health: ${player.health}\n`;
+			str += `>> currentSector: ${player.currentSector}\n`;
+			str += `>> nextSector: ${player.nextSector}\n`;
+			str += `>> nextAction: ${player.nextAction}\n`;
+			str += `>> ----------\n`;
+		}
+		return str;
+	}
+
 	// Internal methods
 
 	// "Increments" the phase, advancing to the next one
 	private advancePhaseState() {
 		switch(this.phase) {
 			case GamePhase.movement:
+				if(this.isFirstMovementPhase) this.isFirstMovementPhase = false;
 				this.phase = GamePhase.interaction;
 				break;
 			case GamePhase.interaction:
@@ -121,7 +212,6 @@ export class Game {
 		let message = `${this.memberRole}, please select a sector to move to.`;
 		if(this.isFirstMovementPhase) {
 			message = `Welcome to the Games.  ${message}`;
-			this.isFirstMovementPhase = false;
 		}
 
 		// Clean up previous message & buttons if they exist
@@ -134,9 +224,27 @@ export class Game {
 
 		this.movementSelectorButtons = new ReactionButtons.ReactionButtonsManager(this.movementSelectorMessage, emojiIdentifiersToUse);
 
-		this.movementSelectorButtons.on("buttonPress", (user, button) => {
-			this.channel.send(`${user} pressed button ${button}`);
-		});
+		this.movementSelectorButtons.on("buttonPress", (user, buttonID) => {this.handleMovementSelectorButtonPress(user,buttonID)});
+	}
+
+	private handleMovementSelectorButtonPress(user: Discord.User, buttonID: number) {
+		let player = this.players.get(user.id);
+
+			if(!player) {
+				// If the user that clicked is not a player,
+				if(this.isFirstMovementPhase) {
+					//If the first movement phase hasn't been executed yet just let them join 
+					const member = this.guild.members.get(user.id);
+					member.addRole(this.memberRole);
+					player = Player.createNewPlayer(this, member)
+					this.players.set(user.id, player);
+				}
+				else return;
+			}
+
+			player.nextSector = buttonID + 1;	//buttons start at 0, sectors start at 1
+
+			console.log(this.dumpGameState());
 	}
 
 	private async runPlayerInteractions() {
@@ -160,8 +268,4 @@ export class Game {
 			}
 		}
 	}
-
 };
-
-
-
