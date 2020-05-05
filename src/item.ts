1288d20e809dfbdff4c6c08196dbfef3b6fd7007
Zachary Damato <zacharyd1@live.com>
2020-05-05 03:34:38 -0400

    tmp: began to define Item classes (before bugs dragged me away)

---
+++ src/item.ts
@@ -0,0 +1,22 @@
+
+
+enum ItemType {
+
+};
+
+abstract class Item {
+	name: string;
+	probabilityToFind: number;
+};
+
+class Weapon extends Item {
+
+};
+
+class Shield extends Item {
+
+};
+
+class Medkit extends Item {
+
+};
